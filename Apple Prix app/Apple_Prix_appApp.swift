f3a76d3f0d1435479196d0b44a54996a7f8e4c91
Frederic <polifredroit@yahoo.fr>
2026-02-18 10:58:04 +0000

    Initial Commit

---
+++ Apple Prix app/Apple_Prix_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Prix_appApp.swift
+//  Apple Prix app
+//
+//  Created by Marcellus Walace on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Prix_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
